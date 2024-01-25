7ec5b364973e6a4332cb9df6c4ccf8b5c5a33a0e
«anna.bayduganova» <baydugova@yandex.ru>
2024-01-25 21:27:06 +0400

    add code

--- Program.cs
+++ Program.cs
@@ -8,3 +8,27 @@
 // [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
+
+
+Console.WriteLine("Введите строки через запятую:");
+string input = Console.ReadLine();
+
+
+string[] inputArray = input.Split(',');
+string[] result = new string[inputArray.Length];
+int count = 0;
+for (int i = 0; i < inputArray.Length; i++)
+{
+    
+    if (inputArray[i].Length <= 3)
+    {
+        result[count++] = inputArray[i];
+    }
+}
+
+Console.WriteLine("Строки дляной 3 или менее символов: ");
+for (int i = 0; i < count; i++)
+{
+    Console.Write(result[i] + " ");
+}
+
